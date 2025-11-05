66a5ad4aad0e58539d0c28ebadbed7af1728426e
KedarPanchal <kedar.t.panchal@outlook.com>
2025-11-05 00:23:53 -0600

    Fix formatting

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -6,7 +6,7 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
-        config = function   ()
+        config = function()
         require('telescope').setup{
             defaults    = {
                     layout_strategy = 'horizontal',
