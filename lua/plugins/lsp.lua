479383f6fd119939cbb46fef4ea16ae0a6b1fa90
KedarPanchal <kedar.t.panchal@outlook.com>
2025-11-12 13:24:54 -0600

    Update lsp config to install packages always

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,10 +7,22 @@ return {
         "neoclide/coc.nvim",
         branch = "release",
         config = function()
+            -- Load CoC extensions
+            vim.g.coc_global_extensions = {
+                "coc-clangd",
+                "coc-css",
+                "coc-go",
+                "coc-html",
+                "coc-json",
+                "coc-lua",
+                "coc-markdownlint",
+                "coc-pyright",
+                "coc-tsserver",
+            }
             -- Use Enter for navigating completion menu (CoC)
             vim.api.nvim_set_keymap('i', '<CR>',
-            [[coc#pum#visible() ? coc#pum#next(1) : "\<CR>"]],
-            { expr = true, silent = true }
+                [[coc#pum#visible() ? coc#pum#next(1) : "\<CR>"]],
+                { expr = true, silent = true }
             )
         end,
     },
