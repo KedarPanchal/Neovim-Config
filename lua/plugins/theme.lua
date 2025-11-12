ca4b546655c39f75da602e84f81c0f67056eb88e
KedarPanchal <kedar.t.panchal@outlook.com>
2025-11-12 13:25:03 -0600

    Add better syntax highlighting

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,7 @@
 -- =========================
 
 return {
+    -- Catppuccin Theme
     {
         "catppuccin/nvim",
         name = "catppuccin",
@@ -12,4 +13,49 @@ return {
             vim.cmd("colorscheme catppuccin-mocha")
         end,
     },
+    -- Treesitter Syntax Highlighting (required for Rainbow Brackets)
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = { "lua", "python", "javascript", "html", "css", "json", "c", "cpp", "go" },
+                highlight = {
+                    enable = true,
+                },
+            }
+        end,
+    },
+    -- Rainbow Brackets
+    {
+        "hiphish/rainbow-delimiters.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            vim.g.rainbow_delimiters = {
+                strategy = {
+                    [""] = "rainbow-delimiters.strategy.global",
+                    vim = "rainbow-delimiters.strategy.local",
+                },
+                query = {
+                    [""] = "rainbow-delimiters",
+                    lua = "rainbow-blocks",
+                },
+                priority = {
+                    [""] = 100,
+                    lua = 210,
+                },
+                highlight = {
+                    "RainbowDelimiterRed",
+                    "RainbowDelimiterYellow",
+                    "RainbowDelimiterBlue",
+                    "RainbowDelimiterOrange",
+                    "RainbowDelimiterGreen",
+                    "RainbowDelimiterViolet",
+                    "RainbowDelimiterCyan",
+                },
+            }
+        end,
+    }
 }
