b3ebd6d4645315f22863f0829e8961fca5dd08a8
KedarPanchal <kedar.t.panchal@outlook.com>
2025-11-21 23:09:42 -0600

    Revert "Revert back to original state"
    This reverts commit c0181af7d61fabed68141585867d839f532bb4c4.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,3 +29,6 @@ vim.api.nvim_set_keymap("i", "[", "[]<Esc>i", { noremap = true })
 vim.api.nvim_set_keymap("i", "\"", "\"\"<Esc>i", { noremap = true })
 vim.api.nvim_set_keymap("i", "'", "''<Esc>i", { noremap = true })
 vim.api.nvim_set_keymap("i", "`", "``<Esc>i", { noremap = true })
+
+-- AI Assistant keymaps
+vim.api.nvim_set_keymap("i", "<C-e>", "<Plug>(copilot-dismiss)", { silent = true })
