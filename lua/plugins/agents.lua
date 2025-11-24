572f19b6c370b44c7b81c3d6426827b25d82717f
KedarPanchal <kedar.t.panchal@outlook.com>
2025-11-24 17:22:09 -0600

    Add avante agent

--- lua/plugins/agents.lua
+++ lua/plugins/agents.lua
@@ -5,22 +5,34 @@
 return {
     { "github/copilot.vim" },
     {
-        "CopilotC-Nvim/CopilotChat.nvim",
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        version = false,
+        lazy = false,
+        opts = {
+            instructions_file = "avante.md",
+            provider = "groq",
+            vendors = {
+                groq = {
+                    __inherited_from = "openai",
+                    api_key_name = "GROQ_API_KEY",
+                    endpoint = "https://api.groq.ai/openai/vi",
+                    model = "openai/gpt-oss-120b",
+                    max_tokens = 65536,
+                    disable_tools = false,
+                },
+            },
+        },
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "github/copilot.vim",
+            {
+                "MunifTanjim/nui.nvim",
+                lazy = true,
+                version = false,
+            },
+            "nvim-telescope/telescope.nvim",
+            "folke/snacks.nvim",
         },
-        build = "make tiktoken",
-        opts = {},
-        config = function()
-            require("CopilotChat").setup({
-                suggestion = {
-                    keymap = {
-                        accept = "<C-Enter>",
-                    },
-                },
-            })
-        end,
+        build = "make",
     }
 }
