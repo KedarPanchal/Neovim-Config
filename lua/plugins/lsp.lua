a3e6e62404ff1be2b470b106565665aa36c868ce
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-28 15:10:54 -0600

    Add LaTeX support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,4 +64,14 @@ return {
             }
         end,
     },
+
+
+    -- LaTeX Support
+    {
+        "lervag/vimtex",
+        lazy = false,
+        config = function()
+            vim.g.vimtex_view_method = 'zathura'
+        end,
+    },
 }
