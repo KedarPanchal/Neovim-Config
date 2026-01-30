186d17f41e59a12247f8fa3715f59b82eaff2e4e
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-30 16:36:48 -0600

    Change LaTeX viewer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -71,7 +71,7 @@ return {
         "lervag/vimtex",
         lazy = false,
         config = function()
-            vim.g.vimtex_view_method = 'zathura'
+            vim.g.vimtex_view_method = 'skim'
             vim.g.vimtex_auto_compile = 1
         end,
     },
