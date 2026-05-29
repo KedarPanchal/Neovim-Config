9d8874e5f8ff9f9bc6ea3db4ad53989fb785cf6e
KedarPanchal <kedar.t.panchal@outlook.com>
2026-05-29 11:41:15 -0500

    Add XCode development support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -94,5 +94,21 @@ return {
           opts = {
             highlighter = { auto_enable = true },
           },
-    }
+    },
+
+    -- Replace XCode
+     {
+        "wojciech-kulik/xcodebuild.nvim",
+        dependencies = {
+          "nvim-telescope/telescope.nvim",
+
+          "MunifTanjim/nui.nvim",
+          "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+        },
+        config = function()
+          require("xcodebuild").setup({
+              -- put some options here or leave it empty to use default settings
+          })
+        end,
+    },
 }
