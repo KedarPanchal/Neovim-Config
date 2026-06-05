8bd54b169a69cf4f60f31792ebe1a8ed3d90d6b0
KedarPanchal <kedar.t.panchal@outlook.com>
2026-06-04 22:45:09 -0500

    Add Haskell LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,6 +14,7 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             vim.lsp.enable("clangd")
+            vim.lsp.enable("hls")
             vim.lsp.enable("lua_ls")
             vim.lsp.enable("pyright")
         end,
